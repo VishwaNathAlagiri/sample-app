27c699102f89552620f47e17e0657e8385500547
vishwan0 <vishwa.n@finastra.com>
2020-06-28 10:47:12 +0530

    create sample app

---
+++ app/precache-manifest.5d5c63c4c8295a221265459722b1c220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96b430449ed06c6f9acaaad66b3d895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bdd6f5b16ca6a4ea8ed",
+    "url": "/static/css/main.fa8f7893.chunk.css"
+  },
+  {
+    "revision": "9d1e52a9a351b362f8e0",
+    "url": "/static/js/2.202d2793.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.202d2793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdd6f5b16ca6a4ea8ed",
+    "url": "/static/js/main.eb0d423c.chunk.js"
+  },
+  {
+    "revision": "acae277f6accb92ac14d",
+    "url": "/static/js/runtime-main.0b98d8c6.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,103 @@
+const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 8001;
+const config = require("./config");
+const app = express();
+const protectedRoutes = express.Router();
+const onTrack = require("./jsonFiles/onTrack.json");
+const onDelayed = require("./jsonFiles/onDelayed.json");
+const onHold = require("./jsonFiles/onHold.json");
+const path = require("path");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", protectedRoutes);
+app.set("Secret", config.secret);
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "app")));
+app.get("/userDetails/:userID", (req, res) => {
+  try {
+    const { userID } = req.params;
+
+    if (userID === "12345") {
+      res.status(200).send({
+        status: 200,
+        message: "Success",
+        data: {
+          title: "CBSE: Grade 5 Maths - Algebra",
+          userName: "Milky James",
+          userNo: "12345",
+          mainTabContent: [
+            "Task",
+            "Conversation",
+            "Files",
+            "Resources",
+            "Status",
+          ],
+          subTabContent: [
+            "List",
+            "Gantt",
+            "Board",
+            "Calendar",
+            "Pivot",
+            "Process",
+          ],
+        },
+      });
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/onTrack/:userID", (req, res) => {
+  try {
+    const { userID } = req.params;
+
+    if (userID === "12345") {
+      res.status(200).send({ status: 200, message: "Success", data: onTrack });
+    } else {
+      res.status(404).send({ message: "Not Found" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/onDelayed/:userID", (req, res) => {
+  try {
+    const { userID } = req.params;
+
+    if (userID === "12345") {
+      res
+        .status(200)
+        .send({ status: 200, message: "Success", data: onDelayed });
+    } else {
+      res.status(404).send({ message: "Not Found" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/onHold/:userID", (req, res) => {
+  try {
+    const { userID } = req.params;
+
+    if (userID === "12345") {
+      res.status(200).send({ status: 200, message: "Success", data: onHold });
+    } else {
+      res.status(404).send({ message: "Not Found" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+let server = http.createServer(app);
+
+server.listen(port, () => console.log("Server Started at port", port));
